cae9804fc61d61f76f1084e935e50716faad094d
Karan Keswani <k_keswan@encs.concordia.ca>
2020-05-01 00:27:24 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import { enableScreens } from 'react-native-screens';
+
+import * as Font from 'expo-font';
+import Navigator from './navigation/Navigator';
+
+enableScreens();
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+  if (!fontLoaded) {
+    return <AppLoading onFinish={() => setFontLoaded(true)} startAsync={fetchFonts} />;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Navigator />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+  }
 });
